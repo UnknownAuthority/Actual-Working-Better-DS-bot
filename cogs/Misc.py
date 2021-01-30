eae8823973c72512483e166358735d4e45280b3a
TheSlayerishere <ghouldrago0@gmail.com>
2021-01-30 11:10:09 +0000

    fixed stuff

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -10,7 +10,7 @@ class Errorhandling(commands.Cog):
   @commands.Cog.listener()
   async def on_ready(self):
     print(f'Logged in as {self.client.user}')
-  @commands.command()
+  #@commands.command()
   #async def embed(self, ctx):
     #await ctx.send(embed = (discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components", color=0xFF5733)))
 
